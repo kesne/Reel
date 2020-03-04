ba67da8c48ff5820b39f20e5300fd714bffacca7
JaimeGensler <jaimegensler0@gmail.com>
2020-03-04 14:57:37 -0800

    Reformat spacing

--- components/Nav/Item.jsx
+++ components/Nav/Item.jsx
@@ -2,15 +2,19 @@ import Link from 'next/link';
 import styled from 'styled-components';
 
 const A = styled.a`
-    display: block;
-    box-sizing: border-box;
     height: 100%;
-    padding: 8px;
+    padding: 8px 16px;
+    box-sizing: border-box;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
     cursor: pointer;
 
     &:hover {
         border-bottom: 3px solid black;
+        padding-top: 11px;
     }
 `;
 
