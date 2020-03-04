5982efd2d9a2d5d9d8b3778a0b2e74077090b9bd
JaimeGensler <jaimegensler0@gmail.com>
2020-03-04 14:57:00 -0800

    Reformat spacing on title

--- components/Nav/Title.jsx
+++ components/Nav/Title.jsx
@@ -2,12 +2,18 @@ import styled from 'styled-components';
 
 const H1 = styled.h1`
     margin: 0;
+    padding: 8px;
+    box-sizing: border-box;
     height: 100%;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 
 export default function Title() {
     return (
-        <div style={{ padding: 8 }}>
+        <div>
             <H1>Reel</H1>
         </div>
     );
