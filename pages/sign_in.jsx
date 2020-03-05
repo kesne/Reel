4e9e7a442fb378b64cfb8dfc609371f46103e726
JaimeGensler <jaimegensler0@gmail.com>
2020-03-05 11:10:03 -0800

    Add basic sign_in page

---
+++ pages/sign_in.jsx
@@ -0,0 +1,9 @@
+import Layout from '../components/Layout';
+
+export default function() {
+    return (
+        <Layout tabTitle="Reel - Sign In">
+            <h1>Sign In Page</h1>
+        </Layout>
+    );
+}
