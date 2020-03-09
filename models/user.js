dbf5d019f45fdc725e588865727c895983fc9c0a
JaimeGensler <jaimegensler0@gmail.com>
2020-03-09 09:50:45 -0700

    Add additional fields to User model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,18 @@ import './connect';
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    passwordHash: String,
+    fullName: String, // "Jaime Gensler"
+    username: String, // "jgensler"
+    passwordHash: String, // "asdflkasjdflkasdjf"
     createdAt: { type: Date, default: Date.now },
+
+    classStanding: Number, // 1 => FR, 2 => SO, ...
+    majors: [String], // ["PHIL"]
+    minors: [String], // ["CRWR"]
+    currentClasses: [{ course: String, title: String, professor: String }],
+
+    tutorTypes: [String], // ["CONTENT", "WRITING"]
+    tutorClasses: [{ course: String, title: String, professor: String }],
 });
 
 export default mongoose.models.User || mongoose.model('User', userSchema);
