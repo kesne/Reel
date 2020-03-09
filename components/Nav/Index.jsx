52501abb9c5cc32a5c8310b53666167c3bbbcbb2
JaimeGensler <jaimegensler0@gmail.com>
2020-03-09 15:46:22 -0700

    Update navbar color

--- components/Nav/Index.jsx
+++ components/Nav/Index.jsx
@@ -10,7 +10,7 @@ const Header = styled.header`
 
     display: flex;
 
-    background-color: #68427d;
+    background-color: #675285;
     border-bottom: 1px solid black;
     box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.5);
 `;
