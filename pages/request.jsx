ad793a6af0c0938f36b404965ea580500dc4d541
JaimeGensler <jaimegensler0@gmail.com>
2020-03-10 16:17:29 -0700

    Begin building initialProps

--- pages/request.jsx
+++ pages/request.jsx
@@ -1,10 +1,28 @@
 import Layout from '../components/Layout';
 import Request from '../components/Request';
+import Router from 'next/router';
+import * as cookies from '../utils/cookies';
+import verifyToken from '../utils/verifyToken';
 
-export default function() {
+export default function RequestPage() {
     return (
         <Layout tabTitle="Reel - Request">
             <Request />
         </Layout>
     );
 }
+
+// RequestPage.getInitialProps = async ctx => {
+//     const rawToken = cookies.get(ctx)('token');
+//     const token = verifyToken(rawToken);
+//     console.log(rawToken);
+//     console.log(token);
+//     if (!token) {
+//         // cookies.remove('token');
+//         Router.push({
+//             pathname: '/sign_in',
+//             query: { from: Router.pathname },
+//         });
+//     }
+//     return {};
+// };
