4c6c5107bff60e6a0a70566b11a8bdfaa4fb60f9
JaimeGensler <jaimegensler0@gmail.com>
2020-03-10 09:28:12 -0700

    Add more descriptive API responses

--- pages/api/sessions.js
+++ pages/api/sessions.js
@@ -1,12 +1,24 @@
-// import User from '../../models/User';
+import User from '../../models/User';
 
 export default async (req, res) => {
     if (req.method === 'POST') {
         const { username, password } = req.body;
         console.log(username, password);
-        res.status(401).json({
-            message: 'No matching username & password found',
+        const user = await User.find({
+            name: username,
+            passwordHash: password,
         });
+
+        if (user.length === 1) {
+            res.status(200).json(user);
+        } else if (user.length > 1) {
+            res.status(500).end();
+            console.log('Multiple users returned from database!');
+        } else {
+            res.status(401).json({
+                message: 'No matching username & password found',
+            });
+        }
     } else {
         res.status(400).json({ message: 'Must use POST for this route' });
     }
